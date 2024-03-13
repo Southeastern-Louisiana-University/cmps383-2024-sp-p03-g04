d545a91e715d8f524201ba442a44edb78e151898
Gavin LeBlanc <leblanc.gavin@yahoo.com>
2024-03-13 13:29:23 -0500

    mobile styling

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import "react-native-gesture-handler";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import LoginScreen from './src/screens/LoginScreen';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Login" screenOptions={{drawerActiveTintColor: "#1261E6"}}>
+                <Drawer.Screen
+                    name="Home"
+                    component={LoginScreen}
+                />
+                <Drawer.Screen
+                    name="Login"
+                    component={LoginScreen}
+                />
+            </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
