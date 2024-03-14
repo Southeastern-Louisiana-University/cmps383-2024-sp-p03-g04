b2a59c43e4d090ba601b79ffb5f3674c7b646271
Gavin LeBlanc <leblanc.gavin@yahoo.com>
2024-03-13 19:49:57 -0500

    fixing deployment

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -18,6 +18,34 @@ export default function App(){
                     component={HomeScreen}
                 />
                 <Drawer.Screen
+                    name="Book A Room"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="Reservations"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="Locations"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="Services"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="Settings"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="About"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
+                    name="Contact Us"
+                    component={HomeScreen}
+                />
+                <Drawer.Screen
                     name="Login"
                     component={LoginScreen}
                 />
--- StickerSmash/src/screens/LoginScreen.js
+++ StickerSmash/src/screens/LoginScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import { View, TextInput, TouchableOpacity, ImageBackground, Alert, StyleSheet, Text } from 'react-native';
+
+const CustomButton = ({ title, onPress }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
@@ -8,7 +14,7 @@ const LoginScreen = () => {
   const handleLogin = async () => {
     try {
       // Send login request to your API
-      const response = await fetch('/api/authentication/login', {
+      const response = await fetch('https://localhost:7116/swagger/v1/swagger.json', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -37,26 +43,38 @@ const LoginScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={text => setUsername(text)}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
+    <ImageBackground
+      source={require('../../assets/images/hotelExterior2.png')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username or E-mail"
+          onChangeText={text => setUsername(text)}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          secureTextEntry
+        />
+        <CustomButton title="Login" onPress={handleLogin} />
+        <CustomButton title="Sign-Up" />
+        <CustomButton title="Forgot Password?" />
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -71,6 +89,21 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     paddingHorizontal: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', // Adjust opacity here
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust opacity here
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 20, 
+    paddingVertical: 5, 
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'blue', // Change text color to blue
   },
 });
 
